995bbc8cfbf02c20bc131c35b2ca4d360e10da92
ericbunese <ericbunese@gmail.com>
2017-10-04 14:56:02 -0300

    Witchcraft

--- processor.cpp
+++ processor.cpp
@@ -4,8 +4,8 @@
 #define associatividade_btb 4
 #define penalidade_miss_btb 8
 #define penalidade_miss_pcpt 8
-#define tamanho_perceptron 32
-#define numero_perceptrons 4
+#define tamanho_perceptron 62
+#define numero_perceptrons 64
 
 // =====================================================================
 processor_t::processor_t() {
@@ -61,7 +61,7 @@ void processor_t::clock() {
 			/// If EOF
 			orcs_engine.simulator_alive = false;
 		}
-		else if (new_instruction.opcode_operation == INSTRUCTION_OPERATION_BRANCH)
+		else if (new_instruction.opcode_operation == INSTRUCTION_OPERATION_BRANCH && new_instruction.branch_type == BRANCH_COND)
 		{
 			perceptron_t *p;
 			//Updates previous perceptron
@@ -69,33 +69,33 @@ void processor_t::clock() {
 			{
 				p = this->PCPT_table->perceptrons[this->PCPT_table->update];
 				this->total_guesses++;
-			 printf("Actual PC: %10.0lld\n", new_instruction.opcode_address);
+			 //printf("Actual PC: %10.0ld\n", new_instruction.opcode_address);
 			 if (this->nextPC!=0)
 			 {
 			  if ((p->taken==1 && this->nextPC!=new_instruction.opcode_address) || (p->taken==-1 && this->nextPC==new_instruction.opcode_address))
 			  {
-						H(1);
+			   H(1);
 			   T(p, p->taken);
 			   this->good_guesses++;
-			   printf("Perceptron Predicted Correctly.\n");
+			   //printf("Perceptron Predicted Correctly.\n");
 			  }
 			  else
 			  {
-			   printf("Perceptron Missed\n");
+			   //printf("Perceptron Missed\n");
 			   this->bad_guesses++;
 			   this->penalties++;
 			   penalidade = penalidade_miss_pcpt;
-						H(-1);
+			   H(-1);
 			   T(p, p->taken*-1);
 			  }
 			 }
 			 this->PCPT_table->update = -1;
-			 printf("==========\n\n");
+			 //printf("==========\n\n");
 			}
 			//Executes the Perceptron
 			int use = (int)new_instruction.opcode_address % numero_perceptrons;
 			p = this->PCPT_table->perceptrons[use];
-			printf("index %d, pointer %p: \n", use, p);
+			//printf("index %d, pointer %p: \n", use, p);
 			P(p, new_instruction);
 		}
 	}
@@ -110,10 +110,10 @@ void processor_t::statistics() {
 	printf("Number of Perceptrons: %d\n", numero_perceptrons);
 	printf("Perceptron Buffer Size: %d\n", tamanho_perceptron);
 
- printf("\nPerceptron good guesses: %lld\nPerceptron bad guesses: %lld\nPenalties: %lld cycles\n", this->good_guesses, this->bad_guesses, this->penalties);
+ printf("\nPerceptron good guesses: %ld\nPerceptron bad guesses: %ld\nPenalties: %ld cycles\n", this->good_guesses, this->bad_guesses, this->penalties);
 	printf("Right guesses: %10.2f\n", ((double)this->good_guesses/(double)this->total_guesses)*100);
 	printf("Wrong guesses: %10.2f\n", ((double)this->bad_guesses/(double)this->total_guesses)*100);
-	printf("Presumed Taken: %lld\nPresumed Not Taken: %lld\n", this->presumed_taken, this->presumed_nottaken);
+	printf("Presumed Taken: %ld\nPresumed Not Taken: %ld\n", this->presumed_taken, this->presumed_nottaken);
 };
 
 void processor_t::P(perceptron_t* p, opcode_package_t inst)
@@ -127,7 +127,7 @@ void processor_t::P(perceptron_t* p, opcode_package_t inst)
 		}
 
 		p->perceptron_output = p->perceptron_weights[0]+sum;
-		printf("Perceptron Output: %lld\n", p->perceptron_output);
+		//printf("Perceptron Output: %ld\n", p->perceptron_output);
 		if (p->perceptron_output<0)
 		{
 		 p->taken = -1;
@@ -139,20 +139,23 @@ void processor_t::P(perceptron_t* p, opcode_package_t inst)
 			this->presumed_taken++;
 		}
 		this->nextPC = inst.opcode_address+inst.opcode_size;
-		printf("Next PC:%10.0lld\n", this->nextPC);
+		//printf("Next PC:%10.0ld\n", this->nextPC);
 		this->PCPT_table->update = p->id;
 	}
 }
 
 void processor_t::T(perceptron_t* p, int64_t t)
 {
+	int64_t n;
 	if (p)
 	{
 		if (sign(p->perceptron_output)!=t || abs(p->perceptron_output)<=0)
 		{
-		 for (int64_t i=0;i<tamanho_perceptron;++i)
+		 for (int64_t i=1;i<tamanho_perceptron;++i)
 		 {
-		  p->perceptron_weights[i] = p->perceptron_weights[i]+t*this->PCPT_table->perceptron_values[i];
+		  n = p->perceptron_weights[i]+t*this->PCPT_table->perceptron_values[i];
+		  if (n<=255 && n>=-256)
+		  	p->perceptron_weights[i] = n;
 		 }
 		}
 	}
@@ -190,7 +193,7 @@ void processor_t::init_PCPT(perceptron_t* p, int id)
 	p->id = id;
 
 	for (int i=0;i<tamanho_perceptron;++i)
-						p->perceptron_weights[i] = 0;
+		 p->perceptron_weights[i] = 0;
 
 	p->perceptron_threshold = 1.93*tamanho_perceptron+14;
 }
